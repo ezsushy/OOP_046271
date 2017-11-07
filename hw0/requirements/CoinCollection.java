658eb79a62018966f477b16b38fda21e521bd8f6
Eitan Ziv <eitan@ziv.co>
2017-11-07 14:38:43 +0200

    Implementation for 3+4 in HW0

--- hw0/requirements/CoinCollection.java
+++ hw0/requirements/CoinCollection.java
@@ -3,11 +3,28 @@
  * Each coin VALUE can only appear in the collection once (e.g only one coin with value 5)
  */
 public class CoinCollection {
-
+	private Wallet wallet;
+	
+	public static void main(String[] args) {
+    	CoinCollection collection = new CoinCollection();
+		Coin c1 = new Coin(1);
+		Coin c5 = new Coin(5);
+		System.out.format("trying to add coin of value %f, results: %s\n", c1.getValue(), (collection.addCoin(c1)? "True":"False"));
+		System.out.format("collection size: %d, collection total: %f\n", collection.getCollectionSize(), collection.getCollectionTotal());
+		System.out.format("trying to add coin of value %f, results: %s\n", c5.getValue(), (collection.addCoin(c5)? "True":"False"));
+		System.out.format("collection size: %d, collection total: %f\n", collection.getCollectionSize(), collection.getCollectionTotal());
+		System.out.format("trying to add coin of value %f, results: %s\n", c5.getValue(), (collection.addCoin(c5)? "True":"False"));
+		System.out.format("collection size: %d, collection total: %f\n", collection.getCollectionSize(), collection.getCollectionTotal());
+		System.out.format("emptying collection\n");
+		collection.emptyCollection();
+		System.out.format("collection size: %d, collection total: %f\n", collection.getCollectionSize(), collection.getCollectionTotal());
+    }
+	
     /**
      * @effects Creates a new coin collection
      */
     public CoinCollection() {
+    	wallet = new Wallet();
     }
     
     /**
@@ -17,18 +34,24 @@ public class CoinCollection {
      * 		   false otherwise
      */
     public boolean addCoin(Coin coin) {
+    	if(wallet.containsCoin(coin.getValue()))
+			return false;
+    	wallet.addCoin(coin);
+		return true;
     }
     
     /**
      * @return the current value of the collection
      */
     public double getCollectionTotal() {
+    	return wallet.getWalletTotal();
     }
     
     /**
      * @return the number of coins in the collection
      */
     public int getCollectionSize() {
+    	return wallet.getWalletSize();
     	
     }
     
@@ -37,5 +60,6 @@ public class CoinCollection {
      * @effects Empties the collection
      */
     public void emptyCollection() {
+    	return wallet.emptyWallet();
     }
 }
--- hw0/requirements/Wallet.java
+++ hw0/requirements/Wallet.java
@@ -2,13 +2,40 @@
  * A wallet can conatain a number of coins. There could be several coins of the same value, 
  * but the same coin cannot apear in the wallet twice
  */
+import java.util.ArrayList;
+
 public class Wallet {
-	
+    private ArrayList<Coin> coins;
+    
+    public static void main(String[] args) {
+		Wallet w = new Wallet();
+		Coin c1 = new Coin(1);
+		Coin c5 = new Coin(5);
+		System.out.format("Wallet size: %d, total: %f\n", w.getWalletSize(), w.getWalletTotal());
+		w.addCoin(c1);
+		w.addCoin(c5);
+		System.out.format("Added two coins, 1 than 5, wallet size: %d, wallet total:%f\n", w.getWalletSize(), w.getWalletTotal());
+		double payed = w.pay(5);
+		System.out.format("called Pay(5) actual pay - %f\n",payed);
+		
+		System.out.format("Wallet size: %d, total: %f\n", w.getWalletSize(), w.getWalletTotal());
+		w.addCoin(c1);
+		w.addCoin(c5);
+		System.out.format("Added two coins, 1 than 5, wallet size: %d, wallet total:%f\n", w.getWalletSize(), w.getWalletTotal());
+		System.out.format("check if Wallet contains coin with value 5, results: %s\n", (w.containsCoin(5)? "True":"False"));
+		payed = w.payMinimum(5);		
+		System.out.format("called PayMinimum(5) actual pay - %f\n",payed);
+		System.out.format("check if Wallet contains coin with value 5, results: %s\n", (w.containsCoin(5)? "True":"False"));
+		System.out.format("Wallet size: %d, total: %f\n", w.getWalletSize(), w.getWalletTotal());
+		w.emptyWallet();
+		System.out.format("Emptied Wallet, new size: %d, new total: %f\n", w.getWalletSize(), w.getWalletTotal());
+	}
+    
     /**
      * @effects Creates a new empty wallet
      */
     public Wallet() {
-    	//
+		coins = new ArrayList<Coin>();
     }
 
 
@@ -19,25 +46,70 @@ public class Wallet {
      * 		   false otherwise
      */
     public boolean addCoin(Coin coin) {
-    	//
+    	coins.add(coin);
+    	return true;
     }
 
     /**
      * @modifies this
      * @effects tries to match at least the sum "sum" with coins in the wallet. 
 	 *			If transaction is possible, removes the paid coins from the wallet; else; changes nothing
+	 * @requires sum < total money in wallet 
      * @return the amount actually paid, 0 if amount could not be obtained
      */
     public double pay(double sum) {
-    	//
+		double cur = 0;
+		ArrayList<Coin> pay_coins = new ArrayList<Coin>();
+		for(Coin C : coins){
+			cur = cur + C.getValue();
+			pay_coins.add(C);
+			if(cur >= sum){
+				for(Coin pay_coin : pay_coins){
+					coins.remove(pay_coin);
+				}
+				return cur;
+			}
+		}
+		return 0;
     }
 
+	
+	/**
+     * @modifies this
+     * @effects tries to match at least the sum "sum" with the minimum number of coins available from the wallet
+	 *			If transaction is possible, removes the paid coins from the wallet; else; changes nothing
+     * @return the amount actually paid, 0 if amount could not be obtained
+     */
+    public double payMinimum(double sum) {
+		double cur = 0;
+		ArrayList<Coin> pay_coins = new ArrayList<Coin>();
+		double[] coin_types = {10, 5, 1, 0.5, 0.1};
+		for(double type : coin_types){
+			for(Coin C : coins){
+				if(C.getValue() == type){
+					cur = cur + C.getValue();
+					pay_coins.add(C);
+					if(cur >= sum){
+						for(Coin pay_coin : pay_coins){
+							coins.remove(pay_coin);
+						}
+						return cur;
+					}
+				}
+			}
+		}
+		return 0;
+    }
 
     /**
      * @return the current total value of coins in the wallet
      */
     public double getWalletTotal() {
-    	//
+    	double sum = 0;
+		for(Coin C : coins){
+			sum = sum + C.getValue();
+		}
+		return sum;
     }
 
 
@@ -45,7 +117,7 @@ public class Wallet {
      * @return the number of coins in the wallet
      */
     public int getWalletSize() {
-    	//
+    	return coins.size();
     }
 
 
@@ -56,7 +128,7 @@ public class Wallet {
 	 * 			if called
      */
     public void emptyWallet() {
-    	//
+    	coins.clear();
     }
 
 
@@ -65,6 +137,10 @@ public class Wallet {
      *  	   false otherwise
      */
     public boolean containsCoin(double value) {
-    	//
+    	for(Coin C : coins){
+			if(C.getValue() == value)
+				return true;
+		}
+		return false;
     }
 }
